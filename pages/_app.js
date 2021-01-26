2431dfd56b302dd58c3c65b1cb24744ddff1e5e9
Felipe Oliveira <felipeoliveira.contato@gmail.com>
2021-01-26 12:54:10 -0300

    First Deploy

--- pages/_app.js
+++ pages/_app.js
@@ -1,26 +1,44 @@
-import { createGlobalStyle, ThemeProvider } from 'styled-components'
+import { createGlobalStyle, ThemeProvider } from 'styled-components'; 
+import db from '../db.json'; 
 
 const GlobalStyle = createGlobalStyle`
+  * {
+    box-sizing: border-box;
+  }
   body {
     margin: 0;
     padding: 0;
-    box-sizing: border-box;
+    /* New styles */
+    display: flex;
+    flex-direction: column;
+    font-family: 'Lato', sans-serif;
+    // Deixa branco no começo
+    background-color: ${({ theme }) => themeDark.colors.bg};
+    color: ${({ theme }) => themeDark.colors.contrastText};
+  }
+  html, body {
+    min-height: 100vh;
+  }
+  #__next {
+    flex: 1;
+    display: flex;
+    flex-direction: column;
   }
 `
 
-const theme = {
-  colors: {
-    primary: '#0070f3',
-  },
-}
+const themeDark = db.themeDark; 
+const themeLight = db.themeLight; 
 
 export default function App({ Component, pageProps }) {
   return (
     <>
-      <GlobalStyle />
-      <ThemeProvider theme={theme}>
+      <ThemeProvider theme={themeDark}>
+        <GlobalStyle />
         <Component {...pageProps} />
       </ThemeProvider>
     </>
   )
 }
+
+
+
